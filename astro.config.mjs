088ba77d725ba223af95c31a29bd585df0eab8db
andiaz <andreas.johansson.dev@gmail.com>
2025-01-18 21:23:07 +0100

    Added sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://andreasjohanssonux.netlify.app/',
   integrations: [
+    sitemap(),
     preact({
       include: ['**/preact/*', '**/ui/*'],
     }),
